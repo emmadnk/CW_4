8aba196919c09dc38a84525ff003f57d3600c065
Viktor Golovkov <karramba@MacBook-Pro-Viktor.local>
2024-07-03 22:23:08 +0300

    create main

--- src/work_with_json.py
+++ src/work_with_json.py
@@ -28,7 +28,7 @@ class WorkWithJSON(AbstractJSON):
         """
         Меитод для записи ваканский в JSON-файл
         """
-        with open("../data/vacancies.json", "w", encoding="utf8") as file:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/vacancies.json", "w", encoding="utf8") as file:
             vacancies_json = json.dumps(vacancies, ensure_ascii=False, indent=4)
             file.write(vacancies_json)
 
@@ -36,15 +36,15 @@ class WorkWithJSON(AbstractJSON):
         """
         Метод для добавления вакансий в файл
         """
-        with open("../data/vacancies.json", "r", encoding="utf8") as file:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/vacancies.json", "r", encoding="utf8") as file:
             list_vacancies = json.load(file)
-        with open("../data/my_vacancies.json", "r", encoding="utf8") as file:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/my_vacancies.json", "r", encoding="utf8") as file:
             list_v = json.load(file)
         for vacancy in list_vacancies:
             if name in vacancy["name"]:
                 list_v.append(vacancy)
         list_vacancies_add = json.dumps(list_v, ensure_ascii=False)
-        with open("../data/my_vacancies.json", "w", encoding="utf8") as file:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/my_vacancies.json", "w", encoding="utf8") as file:
             file.write(list_vacancies_add)
         return list_vacancies_add
 
@@ -52,7 +52,7 @@ class WorkWithJSON(AbstractJSON):
         """
         Метод для получения вакансий по заданным критериям
         """
-        with open("../data/vacancies.json", "r", encoding="utf8") as file:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/vacancies.json", "r", encoding="utf8") as file:
             vacancies = json.load(file)
             criterion_vacancies = []
             for vacancy in vacancies:
@@ -69,5 +69,5 @@ class WorkWithJSON(AbstractJSON):
         """
         list_vacancies_del = []
         list_vac = json.dumps(list_vacancies_del, ensure_ascii=False)
-        with open("../data/my_vacancies.json", "w", encoding="utf8") as f:
+        with open("/Users/karramba/PycharmProjects/pythonProject3/data/my_vacancies.json", "w", encoding="utf8") as f:
             f.write(list_vac)
